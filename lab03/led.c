8b57e6c80b617e10ab392f6cfe65720d37e0ffda
Alana Reyna <reyna@sonoma.edu>
2023-06-12 21:47:21 -0700

    first commit

---
+++ lab03/led.c
@@ -0,0 +1,24 @@
+#include <wiringPi.h>
+#include <stdio.h>
+
+#define LedPin 0
+
+int main(void) {
+  if(wiringPiSetup() == -1) {
+    printf("setup wiringPi failed !");
+    return 1;
+  }
+  printf("linker LedPin : GPIO %d(wiringPi pin)\n", LedPin);
+
+  pinMode(LedPin, OUTPUT);
+
+  while(1) {
+    digitalWrite(LedPin, low);
+    printf("led on...\n");
+    delay(500);
+    digitalWrite(LedPin, HIGH);
+    printf("...led off\n");
+    delay(500);
+  }
+  return 0;
+}
